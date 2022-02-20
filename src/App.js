d2a3bcc67663ede875dcd43fcc68d21280ee64ba
Talha Arıç <talharic@gmail.com>
2022-02-20 03:39:37 +0300

    Added Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import UrlShortener from "./components/UrlShortener";
+import PageRenderer from "./components/UrlShortener";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          talharic
-        </p>
-        <UrlShortener/>
-      </header>
+      <PageRenderer/>
     </div>
   );
 }
--- src/components/UrlShortener.jsx
+++ src/components/UrlShortener.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import Navbar from "./NavigationBar/Navbar";
 const urlShortener = () =>{
+
+    const Pages = [
+        {
+            id:0,
+            path:"/",
+            components: [
+                {
+                    id:0,
+                    name:"Navigation Bar",
+                    component:<Navbar/>
+                }
+            ]
+        }
+    ]
+
     return(
         <div>
-            test
+            {Pages[0].components[0].component}
         </div>
     )
 }
